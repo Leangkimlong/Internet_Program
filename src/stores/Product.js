61440987fd7c8994498b13e591958dc70cabdcd3
Leangkimlong <mizterlong95@gmail.com>
2023-12-05 07:48:21 +0700

    Init

---
+++ src/stores/Product.js
@@ -0,0 +1,186 @@
+import prodImg1 from '../assets/img/Cat-011.png'
+import prodImg2 from '../assets/img/Cms-031.jpg'
+import prodImg3 from '../assets/img/Cms-041.png'
+
+import img1 from '../assets/img/cat-13 1.png'
+import img2 from '../assets/img/cat-11 1.png'
+import img3 from '../assets/img/cat-12 1.png'
+import img4 from '../assets/img/cat-9 1.png'
+import img5 from '../assets/img/cat-3 1.png'
+import img6 from '../assets/img/cat-4 1.png'
+import img7 from '../assets/img/cat-1 4.png'
+import img8 from '../assets/img/cat-15 1.png'
+import img9 from '../assets/img/cat-14 1.png'
+import img10 from '../assets/img/cat-7 1.png'
+
+import proimg1 from '../assets/img/18 1.png'
+import proimg2 from '../assets/img/1 902.png'
+import proimg3 from '../assets/img/3 389454.png'
+import proimg4 from '../assets/img/5 7.png'
+import proimg5 from '../assets/img/7 1.png'
+import proimg6 from '../assets/img/8 1.png'
+import proimg7 from '../assets/img/9 1.png'
+import proimg8 from '../assets/img/11 1.png'
+import proimg9 from '../assets/img/12 1.png'
+import proimg10 from '../assets/img/16 1.png'
+
+
+export default{
+
+    data(){
+        return{
+            Category:[
+                {
+                    img: img1,
+                    itemCount: "14 items",
+                    ItemName: "Cake & Milk",
+                    bgColor: "#F2FCE4"
+                },
+                {
+                    img: img2,
+                    itemCount: "17 items",
+                    ItemName: "Peach",
+                    bgColor: "#FFFCEB"
+                },
+                {
+                    img: img3,
+                    itemCount: "21 items",
+                    ItemName: "Oganic Kiwi",
+                    bgColor: "#ECFFEC"
+                },
+                {
+                    img: img4,
+                    itemCount: "68 items",
+                    ItemName: "Red Apple",
+                    bgColor: "#FEEFEA"
+                },
+                {
+                    img: img5,
+                    itemCount: "34 items",
+                    ItemName: "Snack",
+                    bgColor: "#FFF3EB"
+                },
+                {
+                    img: img6,
+                    itemCount: "25 items",
+                    ItemName: "Black Plum",
+                    bgColor: "#FFF3FF"
+                },
+                {
+                    img: img7,
+                    itemCount: "65 items",
+                    ItemName: "Vegetables",
+                    bgColor: "#F2FCE4"
+                },
+                {
+                    img: img8,
+                    itemCount: "33 items",
+                    ItemName: "Headphone",
+                    bgColor: "#FFFCEB"
+                },
+                {
+                    img: img9,
+                    itemCount: "54 items",
+                    ItemName: "Cake & Milk",
+                    bgColor: "#F2FCE4"
+                },
+                {
+                    img: img10,
+                    itemCount: "63 items",
+                    ItemName: "Orange",
+                    bgColor: "#FFF3FF"
+                },
+            ],
+            Promotion:[
+                {
+                    img: prodImg3,
+                    text: "Everyday Fresh & Clean with Our Products",
+                    bgButton: "#3BB77E",
+                    bgColor: "#F0E8D5"
+                },
+                {
+                    img: prodImg1,
+                    text: "Make your Breakfast Healthy and Easy",
+                    bgButton: "#3BB77E",
+                    bgColor: "#F3E8E8"
+                },
+                {
+                    img:prodImg2,
+                    text: "The best Organic Products Online",
+                    bgButton: "#FDC040",
+                    bgColor: "#E7EAF3"
+                },
+            ],
+            ProItems:[
+                {
+                    img: proimg1,
+                    text: "Seeds of Change Organic Quinoa, Brown, & Red Rice",
+                    
+                    dis: "-17%",
+                    bgColor: "#3BB77E",
+                    show: "active",
+                },
+                {
+                    img: proimg2,
+                    text: "All Natural Italian-Style Chicken Meatballs",
+                    
+                    dis: "Hot",
+                    bgColor: "#FD6E6E",
+                    show: "active",
+                },
+                {
+                    img: proimg3,
+                    text: "Angie’s Boomchickapop Sweet & Salty Kettle Corn",
+                    bgColor: "#FDC040",
+                    dis: "Sale",
+                    show: "active",
+                },
+                {
+                    img: proimg4,
+                    text: "Foster Farms Takeout Crispy Classic Buffalo Wings",
+                    dis: "-17%",
+                    show: "",
+                },
+                {
+                    img: proimg5,
+                    text: "Blue Diamond Almonds Lightly Salted Vegetables",
+                    dis: "-17%",
+                    show: "",
+                },
+                {
+                    img: proimg6,
+                    text: "Chobani Complete Vanilla Greek Yogurt",
+                    dis: "-17%",
+                    show: "",
+                },
+                {
+                    img: proimg7,
+                    text: "Canada Dry Ginger Ale – 2 L Bottle - 200ml - 400g",
+                    dis: "Sale",
+                    show: "active",
+                    bgColor: "#F6C851",
+                },
+                {
+                    img: proimg8,
+                    text: "Encore Seafoods Stuffed Alaskan Salmon",
+                    dis: "-17%",
+                    show: "",
+                },
+                {
+                    img: proimg9,
+                    text: "Gorton’s Beer Battered Fish Fillets with soft paper",
+                    dis: "-17%",
+                    show: "",
+                },
+                {
+                    img: proimg10,
+                    text: "Haagen-Dazs Caramel Cone Ice Cream Ketchup",
+                    dis: "Hot",
+                    show: "active",
+                    bgColor: "#FD6E6E"
+                },
+            ],
+        }
+        
+    }
+}
